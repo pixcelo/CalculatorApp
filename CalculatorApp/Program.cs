3055e8e3e4cae638ff20c2c45baba6fedeed38c9
pixcelo <bodomu@gmail.com>
2024-04-07 13:19:06 +0900

    add: log4net追加

--- CalculatorApp/Program.cs
+++ CalculatorApp/Program.cs
@@ -1,10 +1,27 @@
+using log4net.Config;
+using log4net;
+using System.Runtime.InteropServices;
+
 namespace CalculatorApp
 {
     internal static class Program
-    {        
+    {
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool AllocConsole();
+
         [STAThread]
         static void Main()
-        {     
+        {
+#if DEBUG
+            AllocConsole();
+            XmlConfigurator.Configure(new FileInfo("log4net.config"));
+            ((log4net.Repository.Hierarchy.Hierarchy)LogManager.GetRepository()).Root.RemoveAppender("FileAppender");
+#else
+            XmlConfigurator.Configure(new FileInfo("log4net.config"));
+            ((log4net.Repository.Hierarchy.Hierarchy)LogManager.GetRepository()).Root.RemoveAppender("ConsoleAppender");
+#endif
+
             ApplicationConfiguration.Initialize();
             Application.Run(new CalculatorForm());
         }
